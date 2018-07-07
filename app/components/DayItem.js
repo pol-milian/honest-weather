ba0558637ee48f8ea0a572d9638e762624fb1b14
Polcius <polcius@gmail.com>
2018-07-07 20:35:17 +0800

    added styles

--- app/components/DayItem.js
+++ app/components/DayItem.js
@@ -4,12 +4,22 @@ var getDate = utils.getDate;
 
 import { css } from 'emotion';
 
+const dayContainer = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 1rem;
+`
+
+
+
 
 function DayItem(props) {
   var date = getDate(props.day.dt);
   var icon = props.day.weather[0].icon;
   return (
-    <div onClick={props.onClick}>
+    <div className={dayContainer} onClick={props.onClick}>
       <img
         className={css`
           width: 5rem;
@@ -18,7 +28,7 @@ function DayItem(props) {
         src={'../app/images/weather-icons/' + icon + '.svg'}
         alt="Weather"
       />
-      <h2>{date}</h2>
+      <h2 className={css`color: #338b90;`}>{date}</h2>
     </div>
   )
 }
