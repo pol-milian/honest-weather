7f666c26f0cc58831f30f52017d1305258e27a1b
Polcius <polcius@gmail.com>
2018-07-10 17:49:00 +0800

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
 
 var config = {
   entry: './app/index.js',
@@ -23,19 +22,7 @@ var config = {
       template: 'app/index.html'
     })
   ],
-  mode: 'development',
-
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 };
-
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  )
-}
 
 module.exports = config;
