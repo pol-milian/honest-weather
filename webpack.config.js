ae235822d561811ce10d0f2301704ba5c68f0930
Polcius <polcius@gmail.com>
2018-07-12 23:34:27 +0800

    webpack alternate config test

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 var config = {
   entry: './app/index.js',
@@ -11,15 +12,15 @@ var config = {
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
       { test: /\.(png|svg|jpg|gif)$/, use: [
-        {
-          loader: 'url-loader',
-          options: {
-            limit: 8192
-          }
-        }
-      ]}
+                {
+                  loader: 'url-loader',
+                  options: {
+                    limit: 8192
+                  }
+                }
+              ]}
     ]
   },
   devServer: {
@@ -29,8 +30,59 @@ var config = {
     new HtmlWebpackPlugin({
       template: 'app/index.html'
     })
-  ],
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+  ]
 };
 
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
+
 module.exports = config;
+
+
+
+
+
+// var path = require('path');
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// var config = {
+//   entry: './app/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'index_bundle.js',
+//     publicPath: '/'
+//   },
+//   module: {
+//     rules: [
+//       { test: /\.(js)$/, use: 'babel-loader' },
+//       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+//       { test: /\.(png|svg|jpg|gif)$/, use: [
+//         {
+//           loader: 'url-loader',
+//           options: {
+//             limit: 8192
+//           }
+//         }
+//       ]}
+//     ]
+//   },
+//   devServer: {
+//     historyApiFallback: true,
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: 'app/index.html'
+//     })
+//   ],
+//   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+// };
+
+// module.exports = config;
