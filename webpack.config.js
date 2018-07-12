f8467374c08d422d271ac2f88a56b92405c3c1c6
Polcius <polcius@gmail.com>
2018-07-12 23:41:24 +0800

    revert to default config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
 
 var config = {
   entry: './app/index.js',
@@ -12,15 +11,15 @@ var config = {
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       { test: /\.(png|svg|jpg|gif)$/, use: [
-                {
-                  loader: 'url-loader',
-                  options: {
-                    limit: 8192
-                  }
-                }
-              ]}
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192
+          }
+        }
+      ]}
     ]
   },
   devServer: {
@@ -30,59 +29,8 @@ var config = {
     new HtmlWebpackPlugin({
       template: 'app/index.html'
     })
-  ]
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 };
 
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  )
-}
-
 module.exports = config;
-
-
-
-
-
-// var path = require('path');
-// var HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// var config = {
-//   entry: './app/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'index_bundle.js',
-//     publicPath: '/'
-//   },
-//   module: {
-//     rules: [
-//       { test: /\.(js)$/, use: 'babel-loader' },
-//       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-//       { test: /\.(png|svg|jpg|gif)$/, use: [
-//         {
-//           loader: 'url-loader',
-//           options: {
-//             limit: 8192
-//           }
-//         }
-//       ]}
-//     ]
-//   },
-//   devServer: {
-//     historyApiFallback: true,
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: 'app/index.html'
-//     })
-//   ],
-//   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
-// };
-
-// module.exports = config;
