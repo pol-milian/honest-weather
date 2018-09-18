528c0e614b6c64d434d29d0c50f03c3fb7739c1c
Pol Milian <polcius@gmail.com>
2018-09-18 20:04:46 +0930

    redesign and refactor

--- app/components/Detailed.js
+++ app/components/Detailed.js
@@ -1,8 +1,26 @@
-var React = require('react');
-var DayItem = require('./DayItem');
-var convertTemp = require('../utils/helpers').convertTemp;
-import styled from 'styled-components';
+import styled from "styled-components";
 
+const ReactRouter = require("react-router-dom");
+
+const Link = ReactRouter.Link;
+
+const React = require("react");
+const DayItem = require("./DayItem");
+const convertTemp = require("../utils/helpers").convertTemp;
+
+const BackButton = styled.button`
+  margin: auto;
+  border-radius: 100px;
+  padding: 2rem;
+  border: none;
+  font-size: 4rem;
+  text-decoration: none;
+  width: 400px;
+  color: #fff;
+  position: relative;
+  display: block;
+  background-color: var(--turquoise);
+`;
 
 const DayWrapper = styled.div`
   display: flex;
@@ -13,40 +31,45 @@ const DayWrapper = styled.div`
 `;
 
 const InfoWrapper = styled.div`
-  color: white;
+  margin: 1vh 10vw;
 `;
 
+const WeatherReport = styled.p`
+  font-size: 3rem;
+`;
 
+const MinTemp = styled.span`
+  color: blue;
+  font-weight: 700;
+`;
 
-
-
+const MaxTemp = styled.span`
+  color: red;
+  font-weight: 700;
+`;
 
 class Detailed extends React.Component {
   render() {
-    var props = this.props.location.state;
+    const props = this.props.location.state;
+    const generalForecast = props.weather[0].description;
+    const minDeg = convertTemp(props.temp.min);
+    const maxDeg = convertTemp(props.temp.max);
     return (
       <DayWrapper>
         <DayItem day={props} />
         <InfoWrapper>
-          <p>{props.city}</p>
-          <p>{props.weather[0].description}</p>
-          <p>min temp: {convertTemp(props.temp.min)} degrees C</p>
-          <p>max temp: {convertTemp(props.temp.max)} degrees C</p>
-        </ InfoWrapper>
-      </ DayWrapper>
-    )
+          <WeatherReport>
+            <strong>{generalForecast}</strong> for today. The lowest temperature
+            is <MinTemp>{minDeg} C</MinTemp> and the maximum is{" "}
+            <MaxTemp>{maxDeg} C</MaxTemp>.
+          </WeatherReport>
+        </InfoWrapper>
+        <BackButton onClick={() => this.props.history.goBack()}>
+          Go Back
+        </BackButton>
+      </DayWrapper>
+    );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = Detailed;
