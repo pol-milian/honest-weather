5ef16e3e0e281a3952e2aeac6dbf1e583110134a
Pol Milian <polcius@gmail.com>
2019-06-29 14:50:42 +1000

    tests

--- src/__tests__/Search.test.tsx
+++ src/__tests__/Search.test.tsx
@@ -8,13 +8,15 @@ const handleSubmit = jest.fn();
 
 test('<Search />', () => {
   const { debug, getByTestId, getByText } = render(<Search onSubmit={handleSubmit} />);
-  expect(getByTestId('search')).toBeTruthy();
+  expect(getByTestId('search')).toBeTruthy();  
   fireEvent.change(getByTestId('search'), {
     target: { value: 'london' },
   });
+  
   fireEvent.click(getByText(/search!/i));
+  // debug();
   expect(handleSubmit).toHaveBeenCalledTimes(1);
   expect(handleSubmit).toHaveBeenCalledWith(
     'london');
-  // debug();
+  
 })
